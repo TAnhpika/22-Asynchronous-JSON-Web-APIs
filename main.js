d16018ba4ea3ae486b57b0ce7985afabfe95cde9
TAnhpika <anhsheeran12345@gmail.com>
2026-05-03 20:49:01 +0700

    22.30 Tìm hiểu về Call Stack

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 /**
- * Cấu trúc Execution Context:
- * 1. Variable environment
- * - lưu var và func declaration
- * 2. Lexical environment
- * - Lưu let, const & func parameters
- * 3. This binding (!)
- * - Xđ this tham chiếu về đâu
- * - func: this trỏ về đối tượng gọi nó
- * - arrow func: this trỏ về window (toàn cục)
+ * JS engine có 2 thành phần chính:
+ * 1. Memory Heap (Lưu obj)
+ * 2. Call Stack:
+ * - lưu inf các func đang đc thực thi
+ * - lưu đúng trình tự gọi hàm và xử lý đúng tuần tự -> bản chất JS là chạy đồng bộ
+ * - JS đơn luồng -> 1 call stack
+ *
+ * Stack: cấu trúc dữ liệu ngăn xếp (xếp chồng đĩa)
+ * Last In - First Out
+ *
+ * Call stack: bản chất là lưu trữ các execution context - lun đc push vào Global execution context trc - lun nằm dưới đáy
+ * khi gọi hàm nó sẽ push dần lên. done sẽ pop
  */
+
+function a() {
+    console.log("a");
+    b();
+}
+function b() {
+    console.log("a");
+    c();
+}
+function c() {
+    console.log("a");
+}
+a();
