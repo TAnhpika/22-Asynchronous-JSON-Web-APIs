e376de2dabb5b9a4561ea9a07120fe1addee3c63
TAnhpika <anhsheeran12345@gmail.com>
2026-05-03 08:48:03 +0700

    22.19 Tìm hiểu khái niệm Promise

--- main.js
+++ main.js
@@ -1,55 +1,69 @@
-function sendRequest(method = "GET", url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url, true); // false: gửi đồng bộ - lag -> đơ => true - async
-    xhr.send();
-
-    // trả về sau khi async xong
-    xhr.onload = function () {
-        if ((xhr.status >= 200) & (xhr.status < 400)) {
-            if (typeof callback === "function") {
-                callback(xhr.responseText);
+function sendRequest(method = "GET", url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url, true);
+        xhr.send();
+        xhr.onload = function () {
+            if ((xhr.status >= 200) & (xhr.status < 400)) {
+                resolve(xhr.responseText);
+            } else {
+                reject("Network error.");
             }
-        }
-    };
+        };
+    });
 }
 
 const header = document.querySelector(".header");
 const footer = document.querySelector(".footer");
 const productsList = document.querySelector(".products-list");
 
-sendRequest("GET", "./partials/header.html", (responseText) => {
+sendRequest("GET", "./partials/header.html").then((responseText) => {
     header.innerHTML = responseText;
 });
 
-sendRequest("GET", "./partials/footer.html", (responseText) => {
+sendRequest("GET", "./partials/footer.html").then((responseText) => {
     footer.innerHTML = responseText;
 });
 
-// callback hell: xảy ra khi công việc trước là đầu vào của công việc tiếp theo
-sendRequest(
-    "GET",
-    "https://api01.f8team.dev/api/address/provinces",
-    (response) => {
+// sendRequest("GET", "https://api01.f8team.dev/api/products").then(
+//     (responseText) => {
+//         const response = JSON.parse(responseText);
+//         const products = response.data.items;
+
+//         products.forEach((product) => {
+//             const item = document.createElement("li");
+//             item.textContent = product.title;
+//             productsList.appendChild(item);
+//         });
+//     },
+// );
+
+
+// độc lập logic -> dễ bảo trì, mở rộng
+sendRequest("GET", "https://api01.f8team.dev/api/address/provinces")
+    .then((response) => {
         const provinces = JSON.parse(response).data;
         const firstProvince = provinces[0];
 
-        sendRequest(
+        return sendRequest(
             "GET",
-            `https://api01.f8team.dev/api/address/districts?province_id=${firstProvince.province_id}`,
-            (response) => {
-                const districts = JSON.parse(response).data;
-                const firstDistrict = districts[0];
-
-                sendRequest(
-                    "GET",
-                    `https://api01.f8team.dev/api/address/wards?district_id=${firstDistrict.district_id}`,
-                    (response) => {
-                        const wards = JSON.parse(response).data;
-                        const firstWards = wards[0];
-                        console.log(firstWards);
-                    },
-                );
-            },
+            `https://api01.f8team.dev/api/address/districts123?province_id=${firstProvince.province_id}`,
         );
-    },
-);
+    })
+    .then((response) => {
+        const districts = JSON.parse(response).data;
+        const firstDistrict = districts[0];
+
+        return sendRequest(
+            "GET",
+            `https://api01.f8team.dev/api/address/wards?district_id=${firstDistrict.district_id}`,
+        );
+    })
+    .then((response) => {
+        const wards = JSON.parse(response).data;
+        const firstWards = wards[0];
+        console.log(firstWards);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
