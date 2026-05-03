3264d2ff11cb3065d930899bf6b6c06328b8cd5b
TAnhpika <anhsheeran12345@gmail.com>
2026-05-03 15:18:44 +0700

    22.23 Tìm hiểu về fetch

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-function sendRequest(method = "GET", url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url, true);
-        xhr.send();
-        xhr.onload = function () {
-            if ((xhr.status >= 200) & (xhr.status < 400)) {
-                const contentType = xhr.getResponseHeader("content-type");
-                const isJson =
-                    contentType && contentType.includes("application/json");
+/**
+ * fetch(): hàm giúp thực hiện yêu cầu HTTP ~ xhr. ra đời ở bản js mới hơn nhầm thay thế xhr
+ * có cú pháp ngắn gọn, tiện dụng hơn
+ * hỗ trợ promise (then, catch, async, await) mà k cần new Promise thủ công
+ */
 
-                if (isJson) {
-                    try {
-                        resolve(JSON.parse(xhr.responseText));
-                    } catch (error) {
-                        reject("Invalid JSON format");
-                    }
-                } else {
-                    resolve(xhr.responseText);
-                }
-            } else {
-                reject(`HTTP code: ${xhr.status}`);
-            }
-        };
-        xhr.onerror = () => {
-            reject("Network error.");
-        };
-    });
+// ok: true (yêu cầu gửi thành công & máy chủ k phản hồi lỗi). false (k gửi đc / gửi lỗi)
+// .text(): trả về text / html
+// .json(): trả về json đc auto parse
+// fetch("https://api01.f8team.dev/api/products111")
+//     .then((res) => {
+//         if (!res.ok) throw new Error(`HTTP code: ${res.status}`);
+//         return res.json();
+//     })
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+
+async function sendRequest(method, url) {
+    const res = await fetch(url, { method });
+    if (!res.ok) throw new Error(`HTTP code: ${res.status}`);
+    const type = res.headers.get("content-type");
+    const isJson = type && type.includes("application/json");
+    return isJson ? res.json() : res.text();
 }
 
 const header = document.querySelector(".header");
@@ -48,56 +47,14 @@ sendRequest("GET", "./partials/footer.html")
         console.log(error);
     });
 
-// const timeoutPromise = new Promise((resolve, reject) => {
-//     setTimeout(() => reject("Error."), 2000);
-// });
+sendRequest("GET", "https://api01.f8team.dev/api/products").then(
+    (result) => {
+        const products = result.data.items;
 
-function getFirstProvince() {
-    return sendRequest(
-        "GET",
-        "https://api01.f8team.dev/api/address/provinces",
-    ).then((result) => result.data[0]);
-}
-
-function getFirstDistrict(provinceId) {
-    return sendRequest(
-        "GET",
-        `https://api01.f8team.dev/api/address/districts?province_id=${provinceId}`,
-    ).then((result) => result.data[0]);
-}
-
-function getFirstWard(districtId) {
-    return sendRequest(
-        "GET",
-        `https://api01.f8team.dev/api/address/wards?district_id=${districtId}`,
-    ).then((result) => result.data[0]);
-}
-
-// getFirstProvince()
-//     .then((province) => getFirstDistrict(province.province_id))
-//     .then((district) => getFirstWard(district.district_id))
-//     .then((result) => console.log(result));
-
-/**
- * ES6+: Async, await
- * -> xử lý code bất đồng bộ như code đồng bộ
- * callback -> promise -> ES6+ (trùm cuối về xử lý bất đồng bộ trong JS - phổ biến nhất)
- * await: chờ promise đc result
- *
- * Lưu ý:
- * chỉ dùng với Promise
- * chỉ async function khi có await promise
- * await phải đc đặt ở top level trong body của async function (k bọc trong hàm khác)
- */
-
-function handle() {
-    async function run() {
-        const province = await getFirstProvince();
-        const district = await getFirstDistrict(province.province_id);
-        const ward = await getFirstWard(district.district_id);
-        console.log(ward);
-    }
-    run();
-}
-
-handle();
+        products.forEach((product) => {
+            const item = document.createElement("li");
+            item.textContent = product.title;
+            productsList.appendChild(item);
+        });
+    },
+);
