cfd047f9acca7baa0139ae3e6d566794c6eeb4a5
TAnhpika <anhsheeran12345@gmail.com>
2026-05-04 20:50:35 +0700

    22.35 Fetch hoạt động như thế nào?

--- main.js
+++ main.js
@@ -12,74 +12,41 @@
  * - vòng lặp vô hạn, mỗi lần lặp là 1 tick
  * - mỗi tick sẽ check call stack trống k. nếu trống thì:
  *      + check & xử lý toàn bộ micro task
- *      + nếu micro task sinh ra micro task mới thì vẫn ưu tiên xử lý hết các microtask mới sinh ra ngay trong tick đó
+ *      + nếu micro task sinh ra micro task mới thì:
+ *          - vẫn ưu tiên xử lý hết các microtask mới sinh ra ngay trong tick đó
+ *          - nếu return về micro task nhưng queue còn micro task thì sẽ đc push về cuối queue
  *      + check & xử lý 1 macro task ở đầu hàng để xử lý
  *      + chuyển sang tick tiếp theo
+ * 
+ * fetch:
+ * - thực hiện yêu cầu HTTP bên Web APIs
+ * - phụ thuộc vào tốc độ mạng & phản hồi của máy chủ
+ * -> Web APIs pending (chờ kết quả trả về) 
+ * -> fetch return về 1 Promise pending
+ * -> callback chưa đc đưa vào microtask ngay mà phải chờ Promise resolve r mới đưa vào
+ * -> .then của fetch cx return 1 Promise pending
  */
-// console.log('A');
 
-// setTimeout(() => {
-//     console.log('B');
-// }, 0);
+// 1 6 5 4 2 3
+console.log(1);
 
-// Promise.resolve().then(() => {
-//     console.log('C');
-//     return Promise.resolve();
-// }).then(() => {
-//     console.log('D');
-// });
-
-// setTimeout(() => {
-//     console.log('E');
-// }, 0);
-
-// console.log('F');
-
-
-// Demo Phức Tạp Event Loop
-// A J D
-/**
-micro = [
-() => {
-        return Promise.resolve(); // vì phía sau còn micro task nên sẽ k thực hiện ngay
-    },
-() => {
-    console.log('H');
-    Promise.resolve().then(() => console.log('I'));
-},
-]
-macro = [
-() => {
-    console.log('B');
-    Promise.resolve().then(() => console.log('C'));
-},
-() => console.log('G'),
-() => console.log('E'),
-]
- */
-console.log('A');
+// dù mạng & máy chủ có nhanh đến mấy cx k thể bằng setTimeout 0 và Promise.resolve() ngay lập tức 
+// nếu settimeout lâu hơn thì sẽ chạy sau
+fetch("https://dummyjson.com/users")
+    .then((response) => {
+        console.log(2);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(3);
+    });
 
 setTimeout(() => {
-    console.log('B');
-    Promise.resolve().then(() => console.log('C'));
-}, 0);
-
-Promise.resolve()
-    .then(() => {
-        console.log('D');
-        setTimeout(() => console.log('E'), 0);
-        return Promise.resolve();
-        // return Promise.resolve().then(() => console.log('F')) // TH nà mới thực hiện ngay
-    })
-    // chờ then trc done r then sau ms vào
-    // return r then chỉ đc thực hiện cuối micro task queue
-    .then(() => console.log('F'));
-
-setTimeout(() => console.log('G'), 0);
+    console.log(4);
+}, 1000);
 
 Promise.resolve().then(() => {
-    console.log('H');
-    Promise.resolve().then(() => console.log('I'));
+    console.log(5);
 });
 
-console.log('J');
+console.log(6);
