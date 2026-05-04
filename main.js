1d7421b4401422a1930ea0dc25acf2ba44038606
TAnhpika <anhsheeran12345@gmail.com>
2026-05-04 09:24:02 +0700

    22.31 Cách JavaScript chạy bất đồng bộ

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 /**
- * JS engine có 2 thành phần chính:
- * 1. Memory Heap (Lưu obj)
- * 2. Call Stack:
- * - lưu inf các func đang đc thực thi
- * - lưu đúng trình tự gọi hàm và xử lý đúng tuần tự -> bản chất JS là chạy đồng bộ
- * - JS đơn luồng -> 1 call stack
- *
- * Stack: cấu trúc dữ liệu ngăn xếp (xếp chồng đĩa)
- * Last In - First Out
- *
- * Call stack: bản chất là lưu trữ các execution context - lun đc push vào Global execution context trc - lun nằm dưới đáy
- * khi gọi hàm nó sẽ push dần lên. done sẽ pop
+ * JS bản chất là đồng bộ
+ * -> để chạy bất đồng bộ cần có sự hỗ trợ của môi trường thực thi (JS runtime: trình duyệt / NodeJS) 
+ * -> cung cấp cá thành phần / kn để hỗ trợ   
+ * 1. API: Web APIs (Trình duyệt) / Node APIs (NodeJs)
+ * - Web APIs: 
+ *      + DOM APIs: DOM, DOM Event
+ *      + Media APIs: console, alert, from, confirm ... 
+ *      + Network API: fetch, xhr
+ *      + Timer API: setTimeOut, setInterval
+ * - (bản chất JS engine chỉ có các khái niệm cốt lõi: hàm, biến, kiểu dữ liệu, if else, switch case, loop, context, this ...)
+ * - Node sẽ k có DOM ... vì nó là backend ở server, k phải trình duyệt 
+ * 
+ * khi dùng các API thì Web APIs sẽ xử lý -> done -> đẩy callback xún Task queue
+ * (mỗi cv async đều có 1 callback)
+ * 2. Task Queues: 
+ * - callback đc đưa xún đề chờ đc xử lý
+ * - First In - First out (nhân viên sẽ bán cho người tới trc mua trc)
+ * 3. Event loop (nhân viên): 
+ * - vòng lặp chạy liên tục
+ * - check call stack rảnh k (xử lý hết task, chỉ còn global exe context)
+ * nếu rảnh sẽ lấy 1 cv ở đầu hàng ở task queues để xử lý
+ * 
+ * Lý do trình duyệt tạo ra cơ chế async:
+ * - trình duyệt xử lý các cv liên quan đến giao diện người dùng
+ * - khi xử lý các cv mất tg như setTimeOut, fetch, xhr -> block call stack -> block rendering (bấm nút nhưng k xử lý j cả)
+ * - đẩy sang web API hỗ trợ xử lý đa luồng (chạy task đồng thời) -> tối ưu tg chờ
  */
-
-function a() {
-    console.log("a");
-    b();
-}
-function b() {
-    console.log("a");
-    c();
-}
-function c() {
-    console.log("a");
-}
-a();
+console.log(1);
+setTimeout(() => {
+    console.log(2);
+}, 0);
+setTimeout(() => {
+    console.log(3);
+}, 1000);
+setTimeout(() => {
+    console.log(4);
+}, 0);
+console.log(5);
