b82e3f20a407cda7f0c09ef4c939f1421fc64b77
TAnhpika <anhsheeran12345@gmail.com>
2026-05-04 17:22:43 +0700

    22.33 Ôn luyện thông quá ví dụ đơn giản

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /**
- * Task Queues:
+ * Task Queues (FiFO):
  * - Micro task queue (ưu tiên cao):
  *      + promise.then() / .catch() / .finally
  *      + MutationObserver
@@ -12,68 +12,20 @@
  * - vòng lặp vô hạn, mỗi lần lặp là 1 tick
  * - mỗi tick sẽ check call stack trống k. nếu trống thì:
  *      + check & xử lý toàn bộ micro task
+ *      + nếu micro task sinh ra micro task mới thì vẫn ưu tiên xử lý hết các microtask mới sinh ra ngay trong tick đó
  *      + check & xử lý 1 macro task ở đầu hàng để xử lý
  *      + chuyển sang tick tiếp theo
  */
 
-// console.log(1);
-
-// setTimeout(() => {
-//     console.log(2);
-//     Promise.resolve().then(() => {
-//     console.log(3);
-// });
-// }, 0);
-
-// setTimeout(() => {
-//     console.log(4);
-// }, 0);
-
-// Promise.resolve().then(() => {
-//     console.log(5);
-// });
-
-// Promise.resolve().then(() => {
-//     console.log(6);
-// });
-
-// console.log(7);
-
-const callstack = [];
-const microTask = [
-    () => {
-        console.log(5);
-    },
-    () => {
-        console.log(6);
-    },
-];
-const macroTask = [
-    () => {
+Promise.resolve().then(() => {
+    console.log(1);
+    Promise.resolve().then(() => {
         console.log(2);
-
-        microTask.push(() => {
-            console.log(3);
-        });
-    },
-    () => {
+    });
+});
+Promise.resolve().then(() => {
+    console.log(3);
+    Promise.resolve().then(() => {
         console.log(4);
-    },
-];
-
-let tick = 0;
-
-setInterval(() => {
-    console.log(`Tick: ${++tick}`);
-    if (!callstack.length) {
-        while (microTask.length) {
-            // lấy phần tử đầu tiên
-            const task = microTask.shift();
-            task();
-        }
-        if (macroTask.length) {
-            const task = macroTask.shift();
-            task();
-        }
-    }
-}, 1000);
+    });
+});
