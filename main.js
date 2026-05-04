a8cbbb2c7a817c8b579c4e4d6bac547e87a5687f
TAnhpika <anhsheeran12345@gmail.com>
2026-05-04 18:28:14 +0700

    22.34 Phân tích ví dụ Promise phức tạp hơn

--- main.js
+++ main.js
@@ -16,16 +16,70 @@
  *      + check & xử lý 1 macro task ở đầu hàng để xử lý
  *      + chuyển sang tick tiếp theo
  */
+// console.log('A');
+
+// setTimeout(() => {
+//     console.log('B');
+// }, 0);
+
+// Promise.resolve().then(() => {
+//     console.log('C');
+//     return Promise.resolve();
+// }).then(() => {
+//     console.log('D');
+// });
+
+// setTimeout(() => {
+//     console.log('E');
+// }, 0);
+
+// console.log('F');
+
+
+// Demo Phức Tạp Event Loop
+// A J D
+/**
+micro = [
+() => {
+        return Promise.resolve(); // vì phía sau còn micro task nên sẽ k thực hiện ngay
+    },
+() => {
+    console.log('H');
+    Promise.resolve().then(() => console.log('I'));
+},
+]
+macro = [
+() => {
+    console.log('B');
+    Promise.resolve().then(() => console.log('C'));
+},
+() => console.log('G'),
+() => console.log('E'),
+]
+ */
+console.log('A');
+
+setTimeout(() => {
+    console.log('B');
+    Promise.resolve().then(() => console.log('C'));
+}, 0);
+
+Promise.resolve()
+    .then(() => {
+        console.log('D');
+        setTimeout(() => console.log('E'), 0);
+        return Promise.resolve();
+        // return Promise.resolve().then(() => console.log('F')) // TH nà mới thực hiện ngay
+    })
+    // chờ then trc done r then sau ms vào
+    // return r then chỉ đc thực hiện cuối micro task queue
+    .then(() => console.log('F'));
+
+setTimeout(() => console.log('G'), 0);
 
 Promise.resolve().then(() => {
-    console.log(1);
-    Promise.resolve().then(() => {
-        console.log(2);
-    });
+    console.log('H');
+    Promise.resolve().then(() => console.log('I'));
 });
-Promise.resolve().then(() => {
-    console.log(3);
-    Promise.resolve().then(() => {
-        console.log(4);
-    });
-});
+
+console.log('J');
